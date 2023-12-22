3a912bd6b163fffab69b49cf7a75d53c3e6251c4
zhangguangtong <39423909+923714892@users.noreply.github.com>
2023-12-22 14:14:07 +0800

    Add files via upload

---
+++ One_Stream_Stepwise_Decreasing__for_Visual_Language_Tracking/lib/train/admin/local.py
@@ -0,0 +1,27 @@
+class EnvironmentSettings:
+    def __init__(self):
+        self.workspace_dir = '/home/data/zgt/CVPR2024/OSTrack-main-no-prompt'    # Base directory for saving network checkpoints.
+        self.tensorboard_dir = '/home/data/zgt/CVPR2024/OSTrack-main-no-prompt/tensorboard'    # Directory for tensorboard files.
+        self.pretrained_networks = '/home/data/zgt/CVPR2024/OSTrack-main-no-prompt/pretrained_networks'
+        self.lasot_dir = '/home/data/hxt/data/lasot'
+        self.lasottext_dir = '/home/data/hxt/data/lasot_extension_subset'
+        self.tnl2k_dir = '/home/data/zgt/data/TNL2k/train'
+        self.otb99_dir = '/home/data/zgt/data/OTB_sentences'
+        self.coco_dir = '/home/data/zgt/data/refcoco'
+        self.got10k_dir = '/home/local_data/data/got10k/train'
+        self.got10k_val_dir = '/home/local_data/data/got10k/val'
+        self.lasot_lmdb_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/lasot_lmdb'
+        self.got10k_lmdb_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/got10k_lmdb'
+        self.trackingnet_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/trackingnet'
+        self.trackingnet_lmdb_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/trackingnet_lmdb'
+        self.coco_lmdb_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/coco_lmdb'
+        self.lvis_dir = ''
+        self.sbd_dir = ''
+        self.imagenet_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/vid'
+        self.imagenet_lmdb_dir = '/home/local_data/zgt/CVPR2024/OSTrack-main/data/vid_lmdb'
+        self.imagenetdet_dir = ''
+        self.ecssd_dir = ''
+        self.hkuis_dir = ''
+        self.msra10k_dir = ''
+        self.davis_dir = ''
+        self.youtubevos_dir = ''
